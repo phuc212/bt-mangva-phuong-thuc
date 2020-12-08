83005a75f3fc3e9d3271dcd0eadae78d7ff61ad2
unknown <leconghoangphuc@gmail.com>
2020-12-08 11:06:18 +0700

    tim gia tri trong mang

---
+++ tim_gia_tri_trong_mang/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace tim_gia_tri_trong_mang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] students = {"Christian", "Michael", "Camila", "Sienna", "Tanya", "Connor", "Zachariah", "Mallory", "Zoe", "Emily"};
+            Console.WriteLine("enter the name's student: ");
+            string input_name = Console.ReadLine();
+            bool isExist = false;
+            for(int i = 0; i < students.Length; i++)
+            {
+                if(students[i].Equals(input_name))
+                {
+                    Console.WriteLine("position of the students in the list " + input_name + " is: " + (i + 1));
+                    isExist = true;
+                    break;
+                }
+            }
+            if(!isExist)
+            {
+                Console.WriteLine("not found" + input_name + "in the list");
+            }
+        }
+    }
+}
