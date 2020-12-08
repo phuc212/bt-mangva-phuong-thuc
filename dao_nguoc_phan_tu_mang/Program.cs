8237a14d1c44ca05c3ba68051b437b9469072378
unknown <leconghoangphuc@gmail.com>
2020-12-08 13:49:32 +0700

    reverse array

---
+++ dao_nguoc_phan_tu_mang/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace dao_nguoc_phan_tu_mang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int [] array = new int[20];
+           Random arr = new Random();
+           int temp ;
+           int [] newarray = new int[20];
+            for(int i = 0; i< array.Length; i++)
+            {
+                array[i] = arr.Next(1,21);
+                System.Console.Write($" {array[i]} ");
+            }
+            for(int i = 0, j = array.Length - 1; i < array.Length; i++, j--)
+            {
+                newarray[i] = array[j];
+            }
+            array = newarray;
+            Console.WriteLine("\nreverse array: " );
+           foreach (var value in array)
+           {
+               Console.Write($" {value} ");
+           }
+
+        }
+    }
+}
