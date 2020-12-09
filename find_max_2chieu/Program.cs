561c486686e7fc3050b3669cf52ab8678b6cf834
unknown <leconghoangphuc@gmail.com>
2020-12-09 09:14:26 +0700

    find max 2 chieu

---
+++ find_max_2chieu/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace find_max_2chieu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = {
+                {1,2,3},
+                {2,3,4},
+                {6,7,8}
+            }; 
+            int max = array[0,0];
+            for (int i = 0; i < array.GetLength(0);i++)
+            {
+               for(int j =0; j<array.GetLength(1); j++)
+               {
+                   if(array[i,j]>max)
+                   {
+                       max = array[i,j];
+                   }
+               } 
+            }
+            System.Console.WriteLine(max);
+        }
+    }
+}
