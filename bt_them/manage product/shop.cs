152b56ac7fba69d11325a6d98aa20e956b462d56
unknown <leconghoangphuc@gmail.com>
2020-12-12 15:36:58 +0700

    menu management

---
+++ bt_them/manage product/shop.cs
@@ -0,0 +1,34 @@
+using System;
+namespace manage_product
+{
+    public class Shop
+    {
+        Product[] productlist = new Product[0];
+        public  void AddProduct(string name, double price, string description)
+        {
+            Product product = new Product(name,price,description);
+            Array.Resize(ref productlist,productlist.Length + 1);
+            productlist[productlist.Length -1] = product;
+        }
+        public void RemoveProduct(string name)
+        {
+            for (int i = 0; i < productlist.Length; i++)
+            {
+                if(productlist[i].Name == name)
+                {
+                    for (int j = i; j < productlist.Length -1 ; j++)
+                    {
+                        productlist[j] = productlist[j +1];
+                    }
+                    Array.Resize(ref productlist,productlist.Length -1);
+                }
+            }
+        }
+        public void ShowProduct(){
+            foreach (var item in productlist)
+            {
+                Console.WriteLine(item.infor());
+            }
+        }
+    }
+}
